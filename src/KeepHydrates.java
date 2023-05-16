12c87bd05790d98867bc1af7a737cba542e696e0
Mugabodenys <mugabodan2020@gmail.com>
2023-05-16 16:51:42 +0200

    8 kyu Keep Hydrated

--- src/KeepHydrates.java
+++ src/KeepHydrates.java
@@ -1,5 +1,7 @@
 public class KeepHydrates {
     public static int Liters(double time)  {
+        // int ans = (int) Math.floor(time * 0.5);
+        // return ans;
         return (int) (time * 0.5);
       }
     public static void main(String[] args) throws Exception {
